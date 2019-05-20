a9dddc58ea51f81f9453fcc2a99fad0148e0c1c0
wes35 <lachand_michael@hotmail.com>
2019-05-20 12:18:47 +0200

    projet vierge

---
+++ src/Entity/Sorties.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SortiesRepository")
+ */
+class Sorties
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $nom_sorti;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $datedebut;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $duree;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $datecloture;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbinscriptionsmax;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $descriptionsinfos;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $etatsortie;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomSorti(): ?string
+    {
+        return $this->nom_sorti;
+    }
+
+    public function setNomSorti(string $nom_sorti): self
+    {
+        $this->nom_sorti = $nom_sorti;
+
+        return $this;
+    }
+
+    public function getDatedebut(): ?\DateTimeInterface
+    {
+        return $this->datedebut;
+    }
+
+    public function setDatedebut(\DateTimeInterface $datedebut): self
+    {
+        $this->datedebut = $datedebut;
+
+        return $this;
+    }
+
+    public function getDuree(): ?int
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(?int $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getDatecloture(): ?\DateTimeInterface
+    {
+        return $this->datecloture;
+    }
+
+    public function setDatecloture(\DateTimeInterface $datecloture): self
+    {
+        $this->datecloture = $datecloture;
+
+        return $this;
+    }
+
+    public function getNbinscriptionsmax(): ?int
+    {
+        return $this->nbinscriptionsmax;
+    }
+
+    public function setNbinscriptionsmax(int $nbinscriptionsmax): self
+    {
+        $this->nbinscriptionsmax = $nbinscriptionsmax;
+
+        return $this;
+    }
+
+    public function getDescriptionsinfos(): ?string
+    {
+        return $this->descriptionsinfos;
+    }
+
+    public function setDescriptionsinfos(?string $descriptionsinfos): self
+    {
+        $this->descriptionsinfos = $descriptionsinfos;
+
+        return $this;
+    }
+
+    public function getEtatsortie(): ?int
+    {
+        return $this->etatsortie;
+    }
+
+    public function setEtatsortie(?int $etatsortie): self
+    {
+        $this->etatsortie = $etatsortie;
+
+        return $this;
+    }
+}
